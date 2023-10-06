35c19225b614d0b3fdfa31ccd6d5c6d731c658c9
Ramon Diogo <ramondiogo10.1@gmail.com>
2023-10-06 15:56:37 -0300

    upd: cors to new client url

--- config/prod.exs
+++ config/prod.exs
@@ -9,5 +9,10 @@ config :swoosh, local: false
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :cors_plug,
+  origin: [~r/^https?.*calculator-client-i3ta78y3d-minerado.vercel.app.*$/],
+  max_age: 86400,
+  methods: ["*"]
+
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
