1d4fa164145b0c17a35f07d416efc10dcba550a1
Ramon Diogo <ramondiogo10.1@gmail.com>
2023-10-06 02:25:28 -0300

    upd: configuration

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "localhost"
+  host = System.get_env("APP_NAME") <> ".gigalixirapp.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :calculator, CalculatorWeb.Endpoint,
